7177e8a977cfa4abe3af912a26f822cc6a0e1c0e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-07 12:57:04 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import "./styles.css";
 
+const deck = [
+  { card: "sword" },
+  { card: "wand" },
+  { card: "shield" },
+  { card: "helm" },
+  { card: "hammer" },
+  { card: "ring" },
+];
+
 export default function App() {
+  const shuffleDeck = () => {
+    const shuffledDeck = [...deck, ...deck]
+      .sort(() => Math.random() - 0.5)
+      .map((card, index) => ({ ...card, id: Math.random() }));
+    return shuffledDeck;
+  };
+  console.log(JSON.stringify(shuffleDeck));
   return (
     <div className="App">
       <h1>Memory Game</h1>
